ccefc71242be757e12f539583e011912bab44838
ishini Hettiarachchi <91345814+ishinihettiarachchi@users.noreply.github.com>
2023-03-26 20:01:22 +0530

    admin panel

---
+++ src/datatablesource.js
@@ -0,0 +1,106 @@
+export const userColumns = [
+    {field: "id", headerName:"ID" , width: 70},{
+        field:"user", headerName:"User", width:230, renderCell: (params)=>{
+            return(
+                <div className="cellWithImg">
+                    <img className="cellImg" src={params.row.img} alt="avatar"/>
+                    {params.row.username}
+                </div>
+            )
+            
+        }
+    },
+    {
+        field: "email", headerName: "Email", width: 230
+    },
+    {
+        field: "age", headerName: "Age", width: 100
+    },
+    {
+        field: "status", headerName: "Status", width: 160,
+        renderCell:(params)=>{
+            return(
+                <div className={`cellWithStatus ${params.row.status}`}>
+                    {params.row.status}
+                </div>
+            )
+        }
+
+    }
+]
+
+
+export const userRows = [
+    {
+        id: 1,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 2,
+        username: "Polly",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 3,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "passive",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 4,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "pending",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 5,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 6,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 7,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 8,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+    {
+        id: 9,
+        username: "hillary",
+        img: "https://i.pinimg.com/originals/e0/cc/95/e0cc953fd8068a1a0bff7e260687025d.jpg",
+        status: "active",
+        email: "hhex@gamil.com",
+        age: 21,
+    },
+]
